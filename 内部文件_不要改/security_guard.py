46ed1b05085d2b4b6095866b69fe05653828f5b5
AU56 <986719362@qq.com>
2026-07-14 21:45:51 +0800

    Sync current macOS build file: security_guard.py

--- 内部文件_不要改/security_guard.py
+++ 内部文件_不要改/security_guard.py
@@ -1,20 +1,6 @@
-# -*- coding: utf-8 -*-
 from __future__ import annotations
 
-import os
-import sys
+from raw_loader import exec_raw
 
 
-SUSPICIOUS_ENV_NAMES = {
-    "PYINSTALLER_RESET_ENVIRONMENT",
-    "PYTHONINSPECT",
-}
-
-
-def runtime_guard_ok() -> tuple[bool, str]:
-    if sys.gettrace() is not None:
-        return False, "检测到调试环境，软件已停止运行。"
-    for name in SUSPICIOUS_ENV_NAMES:
-        if os.getenv(name):
-            return False, "运行环境异常，请重新打开软件。"
-    return True, ""
+exec_raw("security_guard.rawcode", globals())
